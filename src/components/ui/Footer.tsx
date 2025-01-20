63f9c4f2c91cd85848024e32dad107c11f8856c2
DuckBat <32737124+duckbat@users.noreply.github.com>
2025-01-20 10:55:03 +0200

    fix footer to match testing

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 const CURRENT_YEAR = new Date().getFullYear();
+const FOOTER_TEXT = "Khai's Wolt assignment";
 
 const Footer: React.FC = () => {
   return (
     <footer className="bg-black text-white py-10 w-full left-0 bottom-0">
       <div className="max-w-7xl mx-auto px-4 text-center">
         <p className="text-sm">&copy; {CURRENT_YEAR}</p>
-        <p className="text-sm">"Khai's Wolt assignment"</p>
+        <p className="text-sm">{FOOTER_TEXT}</p>
       </div>
     </footer>
   );
